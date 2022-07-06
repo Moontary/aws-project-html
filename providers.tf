be9854d9b88a73fa954819432b70df00accdbdde
Moontary <kostiantyntkachenko@gmail.com>
2022-07-06 13:38:28 +0300

    add core logic and additional spec

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "ktkache-dev"
+    key     = "app_ktkache/keyS3"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
+}
+
